9b9fc53d513cef1df885efcb60cce1fc6169c8fd
Simon Cornelius P. Umacob <simoncpu@gmail.com>
2022-11-06 17:13:16 +0800

    Show SendGrid domain auth output.

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "cloudflare_zone" {
   value       = cloudflare_zone.domain
   description = "Cloudflare Zone resource."
 }
+
+output "sendgrid_domain_authentication.domain.id" {
+  value       = sendgrid_domain_authentication.domain
+  description = "SendGrid domain authentication"
+}
